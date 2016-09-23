71f2a6646c1939a376859f1a2deca0cd38087f89
JFreyra <efreyra@stuy.edu>
2016-09-23 14:05:16 -0400

    everything, some issues

--- occu-table.py
+++ occu-table.py
@@ -1,17 +1,38 @@
 from flask import Flask, render_template
+import csv, random
 
 app = Flask(__name__)
-
-links = ["jobs",
-         "tables",
-         "occupations"]
 
 linksT = ["Jobs!",
           "Tables!",
           "Occupation Table!"]
 
+def occDict():
+    with open("occupations.csv", mode="r") as infile:
+        reader = csv.reader(infile)
+        mydict = dict((rows[0],rows[1]) for rows in reader)
+    for(key in mydict):
+        try:
+            float(mydict[k])
+        except ValueError:
+            popitem(key, mydict[k])
+    return mydict
+
+def randOcc():
+    selectjob = float(random.randint(1,total))
+    counter = 0.0;
+    for x in range(0, len(occ)):
+        counter+=float(per[x])*10
+        if(selectjob<=counter):
+            return(occ[x])
+
+occDict = occDict()
+            
 @app.route("/")
 def home():
+    links = ["jobs",
+            "tables",
+            "occupations"]
     return render_template("basic.html",
                            title = "Homepage",
                            heading = "Links: jobs, tables, occupation table!",
@@ -29,7 +50,10 @@ def tables():
 
 @app.route("/occupations")
 def occupations():
-    return "hello"
+    return render_template("table.html",
+                           title = "Occupation Table",
+                           heading = "Occupations; Percentage of Workforce",
+                           varD = occDict)
 
 if(__name__ == "__main__"):
     app.debug = True
